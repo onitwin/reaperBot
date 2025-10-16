39eb6b7e225ffa6daaab13f78893f7c28316f120
onitwin <onitwin@gmail.com>
2025-10-16 19:53:41 +0100

    add celtic cup event

--- commands/events.js
+++ commands/events.js
@@ -5,9 +5,17 @@ const {
   TextDisplayBuilder,
 } = require("discord.js");
 
-const file = new AttachmentBuilder("assets/Clash on the Clyde Event Pack.pdf");
+const cocpdf = new AttachmentBuilder(
+  "assets/Clash on the Clyde Event Pack.pdf"
+);
 
-const file2 = new AttachmentBuilder("assets/cotc.jpeg");
+const celticCupPack = new AttachmentBuilder(
+  "assets/Celtic Cup 2025 Event Pack.pdf"
+);
+
+const coclogo = new AttachmentBuilder("assets/cotc.jpeg");
+
+const celticCupLogo = new AttachmentBuilder("assets/celticCupLogo.jpg");
 
 // const bcpLink = new TextDisplayBuilder().setContent(
 //   "[Best Coast Pairings Link] (https://www.bestcoastpairings.com/event/g34QKVwGBW5N)"
@@ -20,7 +28,7 @@ module.exports = {
     .setName("events")
     .setDescription("Clash on the Clyde Tournament Pack 2025"),
   async execute(interaction) {
-    const embed = new EmbedBuilder()
+    const cotc = new EmbedBuilder()
       .setTitle("Clash on the Clyde 2025 Tournament Pack")
       .setDescription("Missions and info for the 2025 event")
       .setThumbnail("attachment://cotc.jpeg")
@@ -38,11 +46,44 @@ module.exports = {
             "https://www.eventbrite.co.uk/e/clash-on-the-clyde-warhammer-40k-tournament-tickets-1383714771129",
         }
       );
+
+    const ccembed = new EmbedBuilder()
+      .setTitle("Celtic Cup 2025 Tournament Pack")
+      .setDescription("Missions and info for the 2025 event")
+      .setThumbnail("attachment://celticCupLogo.jpg")
+      .setColor("Purple")
+      .addFields(
+        { name: "Date of Event", value: "23/11/2025" },
+        { name: "Time of Event", value: "09:00 AM" },
+        {
+          name: "Best Coast Pairings Link",
+          value: "https://www.bestcoastpairings.com/event/vzvEQEu88Oqf",
+        },
+        {
+          name: "Direct Link to Event Pack",
+          value:
+            "https://drive.google.com/file/d/1dI4fW9NnjaGuzliIDVTpSHjNDNlDM_7w/view?usp=drivesdk",
+        }
+      );
     //can add multiple embeds to embed object
     interaction.reply({
-      embeds: [embed],
-      files: [file, file2],
+      embeds: [cotc, ccembed],
+      files: [cocpdf, coclogo, celticCupLogo, celticCupPack],
       ephemeral: true,
     });
   },
 };
+
+//  .addFields(
+//         { name: "Date of Event", value: "23/11/2025" },
+//         { name: "Time of Event", value: "09:00 AM" },
+//         {
+//           name: "Best Coast Pairings Link",
+//           value: "https://www.bestcoastpairings.com/event/vzvEQEu88Oqf",
+//         },
+//         {
+//           name: "Direct Link to Event Pack",
+//           value:
+//             "https://drive.google.com/file/d/1dI4fW9NnjaGuzliIDVTpSHjNDNlDM_7w/view?usp=drivesdk",
+//         }
+//       );
